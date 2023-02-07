6317beb0fca4a664d98932a6a2b20bf181d11d93
mikiyuito <yuito6001@gmail.com>
2023-02-07 14:20:08 +0900

    画像圧縮追加

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from "vite";
 import path from "path";// https://nodejs.org/api/path.html
 import globule from "globule"; // https://www.npmjs.com/package/globule
 import vitePluginPugStatic from "@macropygia/vite-plugin-pug-static";
+import viteImagemin from "vite-plugin-imagemin";
 
 const inputs = {};
 const documents = globule.find([`./src/**/*.html`, `./src/**/*.pug`], {
@@ -41,6 +42,33 @@ export default defineConfig({
     vitePluginPugStatic({
       buildOptions: { basedir: "src" },
       serveOptions: { basedir: "src" }
+    }),
+    viteImagemin({
+      gifsicle: {
+        optimizationLevel: 7,
+        interlaced: false,
+      },
+      optipng: {
+        optimizationLevel: 7,
+      },
+      mozjpeg: {
+        quality: 20,
+      },
+      pngquant: {
+        quality: [0.8, 0.9],
+        speed: 4,
+      },
+      svgo: {
+        plugins: [
+          {
+            name: "removeViewBox",
+          },
+          {
+            name: "removeEmptyAttrs",
+            active: false,
+          },
+        ],
+      },
     })
   ]
 })
