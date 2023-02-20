b70ae7b98ae174b578fdd40e8ac3b8e783ed41cd
mikiyuito <yuito6001@gmail.com>
2023-02-20 17:09:22 +0900

    Fix:jsファイルからtsファイルに変更したのでパスを変更

--- src/assets/ts/library/setFillHeight.ts
+++ src/assets/ts/library/setFillHeight.ts
@@ -1,4 +1,5 @@
-export const setFillHeight = () => {
+export const setFillHeight = (): string => {
   const vh = window.innerHeight * 0.01;
   document.documentElement.style.setProperty("--vh", `${vh}px`);
+  return "hello";
 };
